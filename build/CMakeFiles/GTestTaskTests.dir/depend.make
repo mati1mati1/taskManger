f27e8089e9658dd061d236dc9cdd34b03b1d577a
matanamichy <datan4@gmail.com>
2025-06-08 09:42:06 +0300

    Remove build directory from repository and add to .gitignore

--- build/CMakeFiles/GTestTaskTests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GTestTaskTests.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/TaskApp.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TaskApp.dir/src/main.cpp.o: \
-  /Users/matanamichy/development/TaskManager/src/main.cpp \
-  /Users/matanamichy/development/TaskManager/include/TaskServer.h
